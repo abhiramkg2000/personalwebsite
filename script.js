7a2fa21b651226dca7b9224bffa28654c3e1ec1d
abhiramkg <48902030+abhiramkg2000@users.noreply.github.com>
2021-10-02 16:27:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const typedTextSpan = document.querySelector(".typed-text");
 const cursorSpan = document.querySelector(".cursor");
 
-const textArray = ["Programmer","WebDesigner", "Gamer", "Passionate"];
+const textArray = ["Programmer","WebDeveloper", "Gamer", "Passionate"];
 const typingDelay = 200;
 const erasingDelay = 100;
 const newTextDelay = 2000; // Delay between current and next text
